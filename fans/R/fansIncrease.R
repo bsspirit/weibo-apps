3b1c5ae8e42d92b3916791a081590d59e32bc017
ConanZhang <zhangdan@tianji.com>
2012-06-06 10:01:05 +0800

    bug for increase

--- fans/R/fansIncrease.R
+++ fans/R/fansIncrease.R
@@ -1,6 +1,6 @@
 #粉丝增长曲线
 
-#uid<-1999250817
+#uid<-2816038140#1999250817
 #path<-paste("image/increase/",uid,".png",sep="")
 
 ## path1<-paste("image/increase/fans-",uid,".png",sep="")
@@ -25,17 +25,17 @@ label <- function(counts){
 png(file=path,height=800)
 par(mfrow=c(3,1),cex=1)
 fans<-query[which(query['type']=='fans'),c(1,2)]
-fans<-rbind(fans,c(20120601,0))
+fans<-rbind(c(20120601,0),fans)
 plot(fans,type="b",lwd=1,xlab="记录日期",ylab="粉丝数量",col="red",xlim=c(20120601,10+max(fans[["date"]])),ylim=c(0,1.5*max(fans[["count"]])),main="粉丝数增长")
 text(fans[["date"]], fans[["count"]]-max(fans[["count"]])/7, label(fans[["count"]]),  col = "blue")
 
 friends<-query[which(query['type']=='friends'),c(1,2)]
-friends<-rbind(friends,c(20120601,0))
+friends<-rbind(c(20120601,0),friends)
 plot(friends,type="b",lwd=1,xlab="记录日期",ylab="关注数量",col="orange",xlim=c(20120601,10+max(friends[["date"]])),ylim=c(0,1.5*max(friends[["count"]])),main="关注数增长")
 text(friends[["date"]], friends[["count"]]-max(friends[["count"]])/7, label(friends[["count"]]),  col = "black")
 
 status<-query[which(query['type']=='status'),c(1,2)]
-status<-rbind(status,c(20120601,0))
+status<-rbind(c(20120601,0),status)
 plot(status,type="b",lwd=1,xlab="记录日期",ylab="发微博数量",col="blue",xlim=c(20120601,10+max(status[["date"]])),ylim=c(0,1.5*max(status[["count"]])),main="发微博数增长")
 text(status[["date"]], status[["count"]]-max(status[["count"]])/7, label(status[["count"]]),  col = "red")
 dev.off()
--- fans/fans-php/protected/controllers/SiteController.php
+++ fans/fans-php/protected/controllers/SiteController.php
@@ -2,20 +2,20 @@
 
 class SiteController extends Controller
 {
-	
+
 	public function actions()
 	{
 		return array(
-			// captcha action renders the CAPTCHA image displayed on the contact page
-// 			'captcha'=>array(
-// 				'class'=>'CCaptchaAction',
-// 				'backColor'=>0xFFFFFF,
-// 			),
-// 			// page action renders "static" pages stored under 'protected/views/site/pages'
-// 			// They can be accessed via: index.php?r=site/page&view=FileName
-// 			'page'=>array(
-// 				'class'=>'CViewAction',
-// 			),
+				// captcha action renders the CAPTCHA image displayed on the contact page
+				// 			'captcha'=>array(
+				// 				'class'=>'CCaptchaAction',
+				// 				'backColor'=>0xFFFFFF,
+				// 			),
+				// 			// page action renders "static" pages stored under 'protected/views/site/pages'
+				// 			// They can be accessed via: index.php?r=site/page&view=FileName
+				// 			'page'=>array(
+				// 				'class'=>'CViewAction',
+				// 			),
 		);
 	}
 
@@ -31,14 +31,14 @@ class SiteController extends Controller
 		}
 		$this->render('index');
 	}
-	
+
 	/**
 	 * 取消授权
 	 */
 	public function actionCallback2(){
-		
+
 	}
-	
+
 	/**
 	 * 授权页
 	 */
@@ -51,12 +51,12 @@ class SiteController extends Controller
 			$keys['redirect_uri'] = Yii::app()->params['WB_CALLBACK_URL'];
 			try {
 				$token = $o->getAccessToken( 'code', $keys ) ;
-// 				var_dump($token);exit;
+				// 				var_dump($token);exit;
 			} catch (OAuthException $e) {
 				var_dump($e);
 			}
 		}
-		
+
 		if (!empty($token)) {//success
 			Yii::app()->session['token'] = $token;
 			setcookie( 'weibojs_'.$o->client_id, http_build_query($token) );
@@ -64,87 +64,92 @@ class SiteController extends Controller
 			$uid=$token['uid'];
 			$expiresIn=$token['expires_in'];
 			$url = 'http://api.fens.me/api/oauth/'.$uid.'?code='.$access.'&expireIn='.$expiresIn.'&state='.$_REQUEST['state'];
-			Yii::app()->session['user']=json_decode(HttpService::get($url));
-			
-			//login
-			$model=new LoginForm;
-			if($model->login()){
-				$this->redirect('/fans');//粉丝管理页面
-			} 
+
+			$u = json_decode(HttpService::get($url));
+			if(!empty($u)){
+				Yii::app()->session['user']=$u;
+					
+				//login
+				$model=new LoginForm;
+				if($model->login()){
+					$this->redirect('/fans');//粉丝管理页面
+				}
+			}
 		}
 		$this->redirect('/');//登录首页
 	}
+
 
 	/**
 	 * This is the action to handle external exceptions.
 	 */
 	public function actionError()
 	{
-	    if($error=Yii::app()->errorHandler->error)
-	    {
-	    	if(Yii::app()->request->isAjaxRequest)
-	    		echo $error['message'];
-	    	else
-	        	$this->render('error', $error);
-	    }
+		if($error=Yii::app()->errorHandler->error)
+		{
+			if(Yii::app()->request->isAjaxRequest)
+				echo $error['message'];
+			else
+				$this->render('error', $error);
+		}
 	}
 
-// 	/**
-// 	 * Displays the contact page
-// 	 */
-// 	public function actionContact()
-// 	{
-// 		$model=new ContactForm;
-// 		if(isset($_POST['ContactForm']))
-// 		{
-// 			$model->attributes=$_POST['ContactForm'];
-// 			if($model->validate())
-// 			{
-// 				$headers="From: {$model->email}\r\nReply-To: {$model->email}";
-// 				mail(Yii::app()->params['adminEmail'],$model->subject,$model->body,$headers);
-// 				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
-// 				$this->refresh();
-// 			}
-// 		}
-// 		$this->render('contact',array('model'=>$model));
-// 	}
+	// 	/**
+	// 	 * Displays the contact page
+	// 	 */
+	// 	public function actionContact()
+	// 	{
+	// 		$model=new ContactForm;
+	// 		if(isset($_POST['ContactForm']))
+	// 		{
+	// 			$model->attributes=$_POST['ContactForm'];
+	// 			if($model->validate())
+	// 			{
+	// 				$headers="From: {$model->email}\r\nReply-To: {$model->email}";
+	// 				mail(Yii::app()->params['adminEmail'],$model->subject,$model->body,$headers);
+	// 				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
+	// 				$this->refresh();
+	// 			}
+	// 		}
+	// 		$this->render('contact',array('model'=>$model));
+	// 	}
 
 	public function actionLogin()
 	{
-// 		$model=new LoginForm;
+		// 		$model=new LoginForm;
 
-// 		// if it is ajax validation request
-// 		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
-// 		{
-// 			echo CActiveForm::validate($model);
-// 			Yii::app()->end();
-// 		}
+		// 		// if it is ajax validation request
+		// 		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
+		// 		{
+		// 			echo CActiveForm::validate($model);
+		// 			Yii::app()->end();
+		// 		}
 
-// 		// collect user input data
-// 		if(isset($_POST['LoginForm']))
-// 		{
-// 			$model->attributes=$_POST['LoginForm'];
-// 			// validate user input and redirect to the previous page if valid
-// 			if($model->validate() && $model->login())
-// 				$this->redirect(Yii::app()->user->returnUrl);
-// 		}
-// 		// display the login form
-// 		$this->render('login',array('model'=>$model));
-		
+		// 		// collect user input data
+		// 		if(isset($_POST['LoginForm']))
+		// 		{
+		// 			$model->attributes=$_POST['LoginForm'];
+		// 			// validate user input and redirect to the previous page if valid
+		// 			if($model->validate() && $model->login())
+		// 				$this->redirect(Yii::app()->user->returnUrl);
+		// 		}
+		// 		// display the login form
+		// 		$this->render('login',array('model'=>$model));
+
 		$this->redirect('/');//转向首页登录
 	}
-	
+
 	public function actionAbout(){
 		$this->render('about');
 	}
-	
+
 	/**
 	 * Logs out the current user and redirect to homepage.
 	 */
 	public function actionLogout()
 	{
 		Yii::app()->session->clear();
-// 		Yii::app()->session->destroy();
+		// 		Yii::app()->session->destroy();
 		Yii::app()->user->logout();
 		$this->redirect('/');
 	}
