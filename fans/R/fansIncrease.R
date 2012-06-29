917af8a2de67ded45cf33ee125e6b95861380b7b
ConanZhang <zhangdan@tianji.com>
2012-06-29 17:23:15 +0800

    up

--- fans/R/fansIncrease.R
+++ fans/R/fansIncrease.R
@@ -1,6 +1,6 @@
 #粉丝增长曲线
-#uid<-1999250817
-#path<-paste("image/increase/",uid,".png",sep="")
+uid<-1999250817
+path<-paste("image/increase/",uid,".png",sep="")
 
 library(ggplot2)
 sql<-paste("SELECT date,count,type FROM t_user_increase t ", 
@@ -11,7 +11,7 @@ source("util/db.R")
 query<-rbind(c(20120601,0,'fans'),c(20120601,0,'friends'),c(20120601,0,'status'),query)
 p<-ggplot(query, aes(x=date,y=count))
 p<-p+geom_line(aes(group=type,colour=type))+geom_point(alpha=0.8,size=2,colour='red')
-p<-p+geom_text(aes(label=count),colour="red",size=4,data=query)
+#p<-p+geom_text(aes(label=count),colour="red",size=4,data=query)
 p<-p+ylab("数量") + xlab("记录日期")
 p<-p+scale_colour_manual("统计变量",values=c('fans'='orange','friends'='green','status'='blue'),labels=c('粉丝数','关注数','微博数'))
 p<-p+opts(title="我的微博增长",legend.title=theme_text(size=20, face="bold"),legend.text=theme_text(size=16))
